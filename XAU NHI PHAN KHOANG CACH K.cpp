0d95e215856d4b405faffab6a331dba34ec3f3d0
Đỗ Hữu Quang <quangcr7235@gmail.com>
2025-07-02 16:42:44 +0700

    Update XAU NHI PHAN KHOANG CACH K.cpp

--- XAU NHI PHAN KHOANG CACH K.cpp
+++ XAU NHI PHAN KHOANG CACH K.cpp
@@ -12,38 +12,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 void kt(int a[100], int n){
-	for(int i=0;i<n;i++){
-		a[i]=0;
-	}
+    for(int i=0;i<n;i++){
+	a[i]=0;
+    }
 }
 int sinh(int a[100], int n){
-	int i=n-1;
-	while(i>=0&&a[i]==1){
-		a[i]=0;
-		--i;
-	}
-	if(i==-1) return 0;
-	else{
-		a[i]=1;
-		return 1;
-	}
+    int i=n-1;
+    while(i>=0&&a[i]==1){
+	a[i]=0;
+	--i;
+    }
+    if(i==-1) return 0;
+    else{
+	a[i]=1;
+	return 1;
+    }
 }
 int main(){
-	int n, k, cnt=0; cin >> n >> k;
-	int a[100];
-	int x=1;
-	kt(a,n);
-	while(x){
-		if(cnt%k==0){
-			for(int i=0;i<n;i++){
-			    cout << a[i];
-		    }
-		    cout << endl;
-		}
-		if(sinh(a,n)){
-			++cnt;
-			x=1;
-		}
-		else x=0;
+    int n, k, cnt=0; cin >> n >> k;
+    int a[100], x=1;
+    kt(a,n);
+    while(x){
+	if(cnt%k==0){
+	for(int i=0;i<n;i++){
+	    cout << a[i];
 	}
+	cout << endl;
+	}
+	if(sinh(a,n)){
+	    ++cnt;
+	    x=1;
+	}
+	else x=0;
+    }
 }
